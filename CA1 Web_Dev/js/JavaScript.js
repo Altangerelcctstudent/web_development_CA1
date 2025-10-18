30a787547e5eb1c7c6639b00f490f7e635a8544d
altangerel <2025146@student.cct.ie>
2025-10-18 20:48:27 +0100

    adding comments
    added essential comments to code

--- CA1 Web_Dev/js/JavaScript.js
+++ CA1 Web_Dev/js/JavaScript.js
@@ -9,40 +9,45 @@ function validateForm(){
     var phoneNumberPattern = /^[0-9\s]/;
     var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]/;
    
-
+    // checking if sure`name field is empty
     if(sureName==""){
         alert("please fill the surename field");
         return false;
     }
+        //checking if client only enters letter
      if(!sureNamePattern.test(sureName)){
         alert("enter only letter pls");
         return false;
     }
+    // checking if first name field is empty
     if(firstName==""){
         alert("please fill the firstname field");
         return false;
     }
+    //checking if client only enters letter
      if(!firstNamePattern.test(firstName)){
         alert("only letter");
         return false;
 
     }
+    //checking ig email address is valid
      if(email=="" || !emailPattern.test(email)){
         alert("please enter a valid email address")
         return false;
 
-    }
+    } 
+    //checking if phone number is valid
     if(phoneNumber==""|| !phoneNumberPattern.test(phoneNumber)){
         alert("please enter a valid phone number")
 
     }
-   
+    // cheking if messege field is empty
     if(question==""){
         alert("please enter ur question or feedback")
         return false;
 
     }
-
+    // giving message about successfully submition
     alert("successfully submitted!!! our staff will be in touch soon, Thank you!!");
     return true;
 
